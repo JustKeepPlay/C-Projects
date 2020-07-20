850870d2379a532eaa90da17929437eef9394d9d
Parinyawat Saenthaweesuk <67275396+JustKeepPlay@users.noreply.github.com>
2020-07-20 17:46:12 +0700

    Add files via upload
    นายปริญญวัฒน์ แสนทวีสุข 6306021610063 IT1-RA

---
+++ HomeworkC++01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << endl;
+    cout << "1st Term of 2020\t\\45132114\tWanthanee Prachuabsupakij" << endl;
+    cout << endl;
+    cout << "----------------------------------------------------------------\n";
+    cout << "| Subject ID | Subject name\t\t| Score (100)|Grade|\n"; 
+    cout << "----------------------------------------------------------------\n";
+    cout << "|  4570034   | Inteoduction to computer\t|     85     | \"A\" |\n";
+    cout << "|  4580247   | English\t\t\t|     74     | \"B\" |\n";
+    cout << "|  4530001   | Design Thinking\t\t|     92     | \"A\" |\n";
+    cout << "----------------------------------------------------------------\n";
+    cout << "|   TOTAL    | Subject = 3\t\t|  Average = 3.87  |\n";
+    cout << "================================================================\n";
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "KMUTNB";
+}
